ea866d0d214c4f0abf45ee9a2e2d1c2d4f183b9d
ayutac <fly.high.android@gmail.com>
2023-12-17 15:54:33 +0100

    Improved the test and added another one.

--- src/test/java/org/abos/common/TestMathUtil.java
+++ src/test/java/org/abos/common/TestMathUtil.java
@@ -6,11 +6,30 @@ import org.junit.jupiter.api.Test;
 public class TestMathUtil {
     
     @Test
+    public void testFactorial() {
+        //noinspection ResultOfMethodCallIgnored
+        Assertions.assertThrows(ArithmeticException.class, () -> MathUtil.factorial(-1));
+        Assertions.assertEquals(1, MathUtil.factorial(0));
+        Assertions.assertEquals(1, MathUtil.factorial(1));
+        Assertions.assertEquals(2, MathUtil.factorial(2));
+        Assertions.assertEquals(6, MathUtil.factorial(3));
+        Assertions.assertEquals(24, MathUtil.factorial(4));
+        Assertions.assertEquals(120, MathUtil.factorial(5));
+        Assertions.assertEquals(720, MathUtil.factorial(6));
+    }
+    
+    @Test
     public void testIsPrime() {
+        Assertions.assertFalse(MathUtil.isPrime(1));
         Assertions.assertTrue(MathUtil.isPrime(2));
         Assertions.assertTrue(MathUtil.isPrime(3));
+        Assertions.assertFalse(MathUtil.isPrime(4));
+        Assertions.assertFalse(MathUtil.isPrime(50));
         Assertions.assertTrue(MathUtil.isPrime(101));
+        Assertions.assertFalse(MathUtil.isPrime(100));
         Assertions.assertTrue(MathUtil.isPrime(43));
+        Assertions.assertTrue(MathUtil.isPrime(-37));
+        Assertions.assertFalse(MathUtil.isPrime(-38));
     }
     
 }
